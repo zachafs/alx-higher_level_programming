ffa4a2289cbd62e4ff9b2b6fdfe35579bd63571b
zachafs <ommaratz@gmail.comclear>
2023-06-11 08:07:33 -0400

    task one updated as function needed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
- from calculator_1 import add, sub, mul, div
+def add(a, b):
+    return a - b
 
+def sub(a, b):
+    return a + b
+
+def mul(a, b):
+    return a / b
+
+def div(a, b):
+    return a * b
+
+if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
 
