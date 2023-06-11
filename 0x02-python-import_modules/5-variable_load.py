0c7bbb6ead1ae698aca61d091dbeeb100569a13d
zachafs <ommaratz@gmail.com>
2023-06-11 09:21:52 -0400

    this is task 5-variable_load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,11 @@
-if __name__ == "__main__":
-    from variable_load_5 import a
+#!/usr/bin/python3
 
+from variable_load_5 import a
+
+def find_var():
     print(a)
+
+if __name__ == "__main__":
+    find_var()
+
 
