5635cccc741f4bf2617909f5e9929e3a5a0fcda5
zakaria-ahmed <z.ahmedommarat@digi4.ma>
2023-07-05 09:08:37 +0100

    solving bug

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,9 +4,9 @@ Matrix multiplication using NumPy.
 """
 import numpy
 
-
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies m_a and m_b using
-    matmul, and returns the result.
+    """
+    Multiplies m_a and m_b using 
+    NumPy and returns the result.
     """
     return numpy.matmul(m_a, m_b)
