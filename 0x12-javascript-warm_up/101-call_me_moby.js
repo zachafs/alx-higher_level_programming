a4d6cea3a54e3ed101f57df8b0651a7b9e85cdd2
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-13 20:13:01 +0100

    Update 101-call_me_moby.js
    -call

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 // Define the callMeMoby function
-function callMeMoby (x, theFunction) {
+function callMeMoby(x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction ();
   }
 }
 
-// Export the callMeMoby function so it can be used in other files
 module.exports.callMeMoby = callMeMoby;
