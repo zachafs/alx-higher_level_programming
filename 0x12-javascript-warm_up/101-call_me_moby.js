50b0e731f03d24e8aebdc6749733dd582c0e29e6
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-13 20:09:00 +0100

    Update 101-call_me_moby.js
    call me bb

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,11 @@
-3
-rts.callMeMoby = function (x, theFunction) {
+#!/usr/bin/node
+
+// Define the callMeMoby function
+function callMeMoby(x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
-};
+}
+
+// Export the callMeMoby function so it can be used in other files
+module.exports.callMeMoby = callMeMoby;
