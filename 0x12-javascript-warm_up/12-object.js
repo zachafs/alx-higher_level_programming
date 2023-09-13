1d0a46435fefeb2172323a1838be863201a04f15
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-13 19:58:39 +0100

    Update 12-object.js
    ind-space

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
+
 const myObject = {
-type: 'object',
-value: 12
+  type: 'object',
+  value: 12
 };
+
 console.log(myObject);
-myObject.value = 89;
+
+myObject.value = 89; // Update the value property
+
 console.log(myObject);
