bf9d8959e96df3a474b4caeea31e52369356f1fc
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-13 20:25:34 +0100

    Update 101-main.js

--- 0x12-javascript-warm_up/101-main.js
+++ 0x12-javascript-warm_up/101-main.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
+// Import the callMeMoby function from the 101-call_me_moby module
 const callMeMoby = require('./101-call_me_moby').callMeMoby;
 
+// Use the callMeMoby function to execute a function 3 times
 callMeMoby(3, function () {
   console.log('C is fun');
 });
