4842dd9e7dd4b125d0195c180f4f51764784ca11
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-13 21:10:58 +0100

    Update 100-map.js
    okey

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+
+const { list } = require('./100-data');
+
+const newList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(list.map((x, xI) => x * xI));
+console.log(newList);
+
