bade66877dbfd9281ea0d2b9b1eb5857c7fb7bc3
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-09-28 20:15:01 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -10,11 +10,13 @@ fi
 url="$1"
 
 # Use curl to send a request to the URL and save the response body to a temporary file
-response=$(curl -s "$url")
+curl -s "$url" > response.tmp
 
 # Calculate the size of the response body in bytes
-size_in_bytes=$(echo -n "$response" | wc -c)
+size_in_bytes=$(stat -c %s response.tmp)
 
 # Display the size in bytes
 echo "$size_in_bytes"
 
+# Remove the temporary file
+rm response.tmp
