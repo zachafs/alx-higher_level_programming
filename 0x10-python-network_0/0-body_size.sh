e1c17dc6cde522fa46e53c5bdad4503967e5663c
zachafs <ommaratz@gmail.com>
2023-09-29 07:40:22 -0700

    Curl will make me crayz

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-# Check if the script is provided with exactly one argument (the URL)
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Get the URL from the command-line argument
-url="$1"
-
-# Use curl to send a request to the URL and save the response body to a temporary file
-response=$(curl -s "$url")
-
-# Calculate the size of the response body in bytes
-size_in_bytes=$(echo -n "$response" | wc -c)
-
-# Display the size in bytes
-echo "$size_in_bytes"
-
+# a script that sends a request to the URL passed in as an argument
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
