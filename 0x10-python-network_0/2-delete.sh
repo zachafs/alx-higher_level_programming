fca02919c99dd99aa73dac51e136b71cfd589a23
zachafs <ommaratz@gmail.com>
2023-09-29 10:49:56 -0700

    echo

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A script that sends a DELETE request to the URL passed and displays the body of the response Send the DELETE request and capture the response in a variable
 response=$(curl -sX DELETE "$1")
-echo "I'm a DELETE reques $response"
+echo "I'm a DELETE request $response"
 
 
