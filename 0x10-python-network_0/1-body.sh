b34be63aa883a5c3ad619994595969a3ce4453eb
zachafs <ommaratz@gmail.com>
2023-09-29 08:40:16 -0700

    # a script that sends a GET request to the URL passed in, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# a script that validates the input URL and sends a GET request to the validated URL, displaying the body of the response
-if [[ $1 =~ ^https?:// ]]; then
-    curl -s "$1" -X GET -L 
-else
-    echo "Invalid URL"
-fi
+# a script that sends a GET request to the URL passed in, and displays the body of the response
+curl -s "$1" -X GET -L 
