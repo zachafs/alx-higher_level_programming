931da5184f92c4fe6d51d2f2d74919beb82f63c6
zachafs <ommaratz@gmail.com>
2023-09-29 09:17:27 -0700

    script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # a script that sends a DELETE request to the URL passed  and displays the body of the response
 response=$(curl -sX DELETE "$1")
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]
+then
     echo "Request successful"
     echo "$response"
 else
