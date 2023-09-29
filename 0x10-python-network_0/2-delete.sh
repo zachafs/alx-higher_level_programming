49998014de1a5a75b1bf725fdef59a23c2147ef3
zachafs <ommaratz@gmail.com>
2023-09-29 09:19:49 -0700

    Im a DELETE request

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # a script that sends a DELETE request to the URL passed  and displays the body of the response
-response=$(curl -sX DELETE "$1")
-if [ $? -eq 0 ]
-then
-    echo "Request successful"
-    echo "$response"
-else
-    echo "Error: Failed to execute curl command"
-fi
+curl -sX DELETE "$1"
+
