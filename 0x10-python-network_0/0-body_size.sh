1ae0c2be0673e18a6480332417336a0d87da9077
zachafs <ommaratz@gmail.com>
2023-09-29 07:56:47 -0700

    Curl will make me crayz

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# a script that sends a request to the URL passed in as an argument
-#Send a request to a specified URL and retrieve the value of the 'Content-Length' header in the request
+# a script that sends a request to the URL passed in as an argumentSend a request to a specified URL and retrieve the value of the 'Content-Length' header in the request
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
+
