813b2e6c5460338bf299e597f1d4ae233248c8f9
zachafs <ommaratz@gmail.com>
2023-09-29 07:55:27 -0700

    Curl will make me crayz

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # a script that sends a request to the URL passed in as an argument
-#Send a request to a specified URL and retrieve the value of the 'Content-Length' header in the response.
-#Usage:
-#   ./script.sh [URL]
-#Args:
-#   URL (str): The URL to send the request to.
-#Returns:
-#   str: The value of the 'Content-Length' header in the response.
-#Example:
-#    ./script.sh https://example.com
+#Send a request to a specified URL and retrieve the value of the 'Content-Length' header in the request
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
